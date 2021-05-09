a00e291951d8b21df0e7b80f508b18804a5b7a01
Ali Razzaq <alirazzaquet@gmail.com>
2021-05-09 16:15:42 +0500

    Uber Driver app is initiated

---
+++ UberDriverClone/App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView >
+      <StatusBar  />
+      < Text> Hello World! </Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
